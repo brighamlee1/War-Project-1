8883096f5abf6d8231aca7ebc76385418459333d
brighamlee1 <brighamlee1@gmail.com>
2022-04-08 12:52:56 -0500

    Creating shuffle function and splitshuffle function

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const suits = ['♣', '♦', '♥', '♠'];
 const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 let deck = [];
 
+// creating new deck function so we can eventually just create new deck
 function newDeck() {
     ranks.forEach(function (rank) {
         suits.forEach(function (suit) {
@@ -22,4 +23,21 @@ function newDeck() {
         })
     })
 }
-newDeck()
+
+newDeck();
+
+// shuffling the deck we made above^^
+function shuffle() {
+    for (let i = deck.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * i);
+        let random = deck[i];
+        deck[i] = deck[j];
+        deck[j] = random;
+    }
+}
+
+// 
+function splitShuffle() {
+    const deckSplit = Math.ceil(deck.length / 2)
+    const player1Deck = 
+}
