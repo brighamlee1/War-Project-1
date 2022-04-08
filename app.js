bee4c801ae0f83e325f5cbba992d5937319677e5
brighamlee1 <brighamlee1@gmail.com>
2022-04-08 11:10:32 -0500

    creating function to make deck with all of the ranks and suits needed

--- app.js
+++ app.js
@@ -4,28 +4,22 @@ let player1CardsWon = document.querySelector('.player1CardsWon');
 let player1Cards = document.querySelector('.player1DCards');
 let player2Deck = document.querySelector('.player2Deck');
 let player2CardsWon = document.querySelector('.player2CardsWon');
-let player1Cards = document.querySelector('.player1Cards');
+let player2Cards = document.querySelector('.player2Cards');
 
-// creating deck class
-class deck {
-    constructor(cards) {
-        this.cards = cards
-    }
-}
-
-// creating card class
-class card {
-    constructor(suit, rank) {
-        this.suit = suit;
-        this.rank = rank;
-    }
-}
-
+// creating suits and ranks
 const suits = ['♣', '♦', '♥', '♠'];
 const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+let deck = [];
 
 function newDeck() {
-    for (let i = 0; i <= suits.length; i++) {
-        suits[i].length
-    }
+    ranks.forEach(function (rank) {
+        suits.forEach(function (suit) {
+            let card = {
+                rank: rank,
+                suit: suit,
+            }
+            deck.push(card);
+        })
+    })
 }
+newDeck()
