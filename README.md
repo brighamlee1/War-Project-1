2fccf69795119716f9e9fb3b2d911e13861d7f6f
brighamlee1 <brighamlee1@gmail.com>
2022-04-11 14:24:46 -0500

    Adding a modal description to my game

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # War-Project-1
 
-## Description
 - War is a card game where a deck of 52 cards is split between two people
 - Both players will play a card and whichever is the highest rank wins
 - If both players play the same ranked card, a war begins
@@ -9,7 +8,7 @@
 - The game ends when one player is completely out of cards.
 
 ## Wireframe 
-![WarWireframe](https://user-images.githubusercontent.com/91819733/162468920-b92fc1a1-126a-40b3-bc8b-f63f3e7e4822.png)
+![Screen Shot 2022-04-11 at 2 23 16 PM](https://user-images.githubusercontent.com/91819733/162814399-852612d0-d55b-417a-8aee-67ac3a188d94.png)
 
 ## User Stories
 - As a user, I want to click a card and have it flipped over and then collect the cards if mine ranks higher than my opponents.
--- app.js
+++ app.js
@@ -12,6 +12,8 @@ let draw = document.querySelector('.draw');
 let winnerText = document.querySelector('.text');
 let cards1 = document.querySelector('.cards1');
 let cards2 = document.querySelector('.cards2');
+let openDescriptionButton = document.querySelectorAll('[data-modal-target]');
+let closeDescriptionButton = document.querySelectorAll('[data-close-button]');
 
 // creating suits and ranks
 const cardAttributes = {
@@ -61,7 +63,7 @@ function shuffle() {
 // split the deck between player1 and player 2
 function deckSplit() {
     const deckSplit = Math.ceil(deck.length / 2);
-    player1Deck = deck.slice(25, deckSplit);
+    player1Deck = deck.slice(24, deckSplit);
     player2Deck = deck.slice(deckSplit, deck.length);
     winner();
 }
@@ -107,7 +109,7 @@ function color2Change() {
     }
 }
 
-// drawing cards and calling the function above
+// drawing cards by calling the flip card function
 draw.addEventListener('click', playerFlip);
 
 
@@ -138,9 +140,6 @@ function compareCards() {
                 player1Deck.push(player2Deck[7]);
                 player1Deck.splice(0, 8);
                 player2Deck.splice(0, 8);
-                cards1.innerText = `${player1Deck[7].rank} ${player1Deck[7].suit}`;
-                cards2.innerText = `${player2Deck[7].rank} ${player2Deck[7].suit}`;
-                winnerText.innerText = 'Player 1 Won the war!';
             }
             else if (player2Deck[7].cardRank > player1Deck[7].cardRank) {
                 player2Deck.push(player1Deck[0]);
@@ -161,9 +160,6 @@ function compareCards() {
                 player2Deck.push(player2Deck[7]);
                 player1Deck.splice(0, 8);
                 player2Deck.splice(0, 8);
-                cards1.innerText = `${player1Deck[7].rank} ${player1Deck[7].suit}`;
-                cards2.innerText = `${player2Deck[7].rank} ${player2Deck[7].suit}`;
-                winnerText.innerText = 'Player 2 Won the war!';
             }
         }
         else if (player1Deck[3].cardRank > player2Deck[3].cardRank) {
@@ -177,9 +173,6 @@ function compareCards() {
             player1Deck.push(player2Deck[3]);
             player1Deck.splice(0, 4);
             player2Deck.splice(0, 4);
-            cards1.innerText = `${player1Deck[3].rank} ${player1Deck[3].suit}`;
-            cards2.innerText = `${player2Deck[3].rank} ${player2Deck[3].suit}`;
-            winnerText.innerText = 'Player 1 Won the war!';
         }
         else if (player2Deck[3].cardRank > player1Deck[3].cardRank) {
             player2Deck.push(player1Deck[0]);
@@ -192,9 +185,6 @@ function compareCards() {
             player2Deck.push(player2Deck[3]);
             player1Deck.splice(0, 4);
             player2Deck.splice(0, 4);
-            cards1.innerText = `${player1Deck[3].rank} ${player1Deck[3].suit}`;
-            cards2.innerText = `${player2Deck[3].rank} ${player2Deck[3].suit}`;
-            winnerText.innerText = 'Player 2 Won the war!';
         }
     }
     else if (player1Deck[0].cardRank > player2Deck[0].cardRank) {
@@ -249,8 +239,43 @@ function endGame() {
     cards1.classList.add('white');
     cards1.classList.remove('red');
     cards1.classList.remove('black');
-    cards2.innerHTML = ``;
+    cards2.innerHTML = "Game Over!!";
+    cards2.classList.add('blue');
+    cards2.classList.remove('red');
+    cards2.classList.remove('black');
     cards2.classList.remove('cardHolder2');
     draw.removeEventListener('click', playerFlip);
-    alert("Game Over! Refresh the page and then press start to play again!");
+}
+
+
+// creating modal for my game
+
+// opening the modal
+openDescriptionButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    })
+})
+
+// closing the modal
+closeDescriptionButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    })
+})
+
+// function to make the opening of the modal happen
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+// function to make the closing of the modal happen
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
 }
